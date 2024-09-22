e7213c66428c4c73c90bc48c43a2efa75a5d256d
MrBarker47 <jebarker7@gmail.com>
2024-09-22 13:30:15 -0400

    Update files

--- HTML_CSS/AdminPage/index.js
+++ HTML_CSS/AdminPage/index.js
@@ -1,5 +1,5 @@
 const ctx = document.getElementById('myChart');
-const game = document.getElementById('gameChart')
+const game = document.getElementById('chart-container')
 
   new Chart(ctx, {
     type: 'bar',
@@ -21,3 +21,43 @@ const game = document.getElementById('gameChart')
   });
 
 //   Second Chart
+const dataSource = {
+  chart: {
+    caption: "Market Share of Web Servers",
+    plottooltext: "<b>$percentValue</b> of web servers run on $label servers",
+    showlegend: "1",
+    showpercentvalues: "1",
+    legendposition: "bottom",
+    usedataplotcolorforlabels: "1",
+    theme: "candy"
+  },
+  data: [
+    {
+      label: "Apache",
+      value: "32647479"
+    },
+    {
+      label: "Microsoft",
+      value: "22100932"
+    },
+    {
+      label: "Zeus",
+      value: "14376"
+    },
+    {
+      label: "Other",
+      value: "18674221"
+    }
+  ]
+};
+
+FusionCharts.ready(function() {
+  var myChart = new FusionCharts({
+    type: "pie2d",
+    renderAt: "chart-container",
+    width: "100%",
+    height: "100%",
+    dataFormat: "json",
+    dataSource
+  }).render();
+});
