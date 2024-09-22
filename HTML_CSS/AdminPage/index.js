20ab90c21878d33b3bd863e1a3761f6ee2bfa008
MrBarker47 <jebarker7@gmail.com>
2024-09-21 22:24:03 -0400

    Update files

---
+++ HTML_CSS/AdminPage/index.js
@@ -0,0 +1,24 @@
+const ctx = document.getElementById('myChart');
+const ctx2 = document.getElementById('newChart')
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Apex', 'Nba2k25', 'College Football25', 'MK8', 'Madden25', 'COD6'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+//   Second Chart
+
