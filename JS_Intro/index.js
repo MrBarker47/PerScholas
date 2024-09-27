cdfecfa52842d3c85419ea9c4f5d29b5d296f371
MrBarker47 <jebarker7@gmail.com>
2024-09-26 23:17:55 -0400

    Update files

--- JS_Intro/index.js
+++ JS_Intro/index.js
@@ -1,13 +1,13 @@
 // The initial numbers that must be verified.
-const n1 = 10;
-const n2 = 15;
-const n3 = 20;
-const n4 = 5;
+// const n1 = 10;
+// const n2 = 15;
+// const n3 = 20;
+// const n4 = 5;
 
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
-const isSum50 = (n1 + n2 + n3 + n4) == 50;
+// const isSum50 = (n1 + n2 + n3 + n4) == 50;
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
@@ -18,7 +18,7 @@ const isSum50 = (n1 + n2 + n3 + n4) == 50;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-// const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;/\
+// const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -43,3 +43,40 @@ const isSum50 = (n1 + n2 + n3 + n4) == 50;
 //   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
 //   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
 //   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+
+
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+
+//Part 1
+//Checking if the number are divisible 5
+let divisible = (n1 % 5 === 0) || (n2 % 5 === 0) || (n3 % 5 === 0) ||  (n4 % 5 === 0) >= 5;
+console.log(divisible);
+
+let check = n1 > n4;
+console.log(check);
+
+let math = n1 - n2;
+console.log(math);
+
+let math2 = math * n3;
+console.log(math2);
+
+let newMath = math2 / n4;
+console.log(newMath);
+
+const isOver25 = (!n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25);
+console.log(isOver25);
+
+// Part 2 
