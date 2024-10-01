acf0efcb6bba5a61586b24e7cd48ca7d144ad15a
MrBarker47 <jebarker7@gmail.com>
2024-10-01 15:50:29 -0400

    Updated the files

--- JS_Intro/Control-Flow/control-flow.js
+++ JS_Intro/Control-Flow/control-flow.js
@@ -1,29 +1,40 @@
-// Part 1: Growing Pains
-const PI = 3.14;
+//Part 1
+const PI = 3.1415;
 const radius = 5;
 const area = PI * radius * radius;
-let growth  = area;
-let newArea = 0.8;
+const growth = area;
+const meters = 0.8;
+
+
 if(growth > 80) {
-    console.log("Pruned") 
+    console.log("Pruned");
 } else if(growth > 50) {
     console.log("Monitored");
-} else if (growth < 30) {
+} else if(growth < 50) {
     console.log("Planted");
-} else {
-    console.log("");
 }
 
 
-
-//Part 2: Thinking Bigger
+//Part 2
 let plants = 100;
-let weeks = plants ** 10;
-let garden = weeks * newArea;
-function sqaure() {
-    return Math.sqrt(plants / PI);
+let weeks = 10;
+let garden_Expand = meters * weeks;
+let n = weeks / PI;
+let s = Math.sqrt(n);
+console.log(s);
+
+
+//Part 3
+try {
+    if(growth > 80) {
+        console.log("Pruned");
+    } else if(growth > 50) {
+        console.log("Monitored");
+    } else if(growth < 50) {
+        console.log("Planted");
+    }
+}
+catch(error) {
+    console.log(error);
 }
 
-console.log(sqaure());
-
-//Part 3: Errors in Judgement 
