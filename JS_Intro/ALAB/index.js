bd9f6c114246b6cbd20df5a317cf4660cecef640
MrBarker47 <jebarker7@gmail.com>
2024-10-01 23:50:22 -0400

    Update files

--- JS_Intro/ALAB/index.js
+++ JS_Intro/ALAB/index.js
@@ -1,22 +1,44 @@
-//Part 1
-// let fizz = 3;
-// let Buzz = 5;
+// Part 1
+let fizz = 3;
+let Buzz = 5;
 
-// for(let i = 1; i <= 100; i++) {
-//   console.log(i);
-//   if(i % fizz === 0) {
-//     console.log("Fizz");
+for(let i = 1; i <= 100; i++) {
+  console.log(i);
+  if(i % fizz === 0) {
+    console.log("Fizz");
 
-//   } else if (i % Buzz === 0) {
-//        console.log("Buzz");
+  } else if (i % Buzz === 0) {
+       console.log("Buzz");
     
-//    } else if (i % 3 && 5) {
-//        console.log("FizzBuzz");
+   } else if (i % 3 && 5) {
+       console.log("FizzBuzz");
 
-//       } 
+      } 
 
-// }
+}
 
-//Part 2 Prime Time
-// let n = 2;
-// let prime = "";
+//Part 2
+function isPrime(number) {
+  let n = 5;
+  for(let i = 2; i <= number; i++) {
+    if(number % i ==0) {
+      n = n + 1;
+    }
+  }
+  if(n == 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPrime());
+
+//Part 3
+let cell0 = "ID,Name,Occupation,Age \n"
+ let cell1 = "42, Bruce, Knight, 41  \n"
+ let cell2 =  "57, Bob, Fry Cook, 19 \n"
+ let cell3 =  "63, Blaine, Quiz Master, 58 \n"
+ let cell4 ="98, Bill, Doctor's Assistant, 26 \n"
+
+       
+ console.log(cell1,cell2,cell3,cell4);
