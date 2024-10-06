f7e35e5dd2099feba60b114495a2a9385e649629
MrBarker47 <jebarker7@gmail.com>
2024-10-06 03:57:04 -0400

    Update files

--- JS_Intro/newLab/lab.js
+++ JS_Intro/newLab/lab.js
@@ -1,11 +1,37 @@
 //Part 1 Thinking Functionally 
-function numbers() {
+//Take array of numbers and return the sum.
+function array() {
     let list = [1,2,3,4,5,6,7];
     let sum = 0;
     for(let i = 0; i < list.length; i++) {
-        return sum += list.length;
+        list.forEach((arr) => sum += arr);
+        }
+      console.log(sum);
     }
+array();
 
+//Take array of numbers and return the average.
+function newArray() {
+    let newList = [1,2,3,4];
+    let avg = 0;
+    for(let i = 0; i < newList.length; i++) {
+        newList.forEach((count) => avg - count);
+    }
+    console.log(avg);
 }
 
-console.log(numbers());
+newArray();
+
+//Take an array of strings and return the longest string
+function str() {
+    let newString = ['Money Talks', 'Friday', 'Rush Hour 2', 'Low Down Dirty Shame'];
+    let long = ' ';
+    for(let i = 0; i < newString.length; i++) {
+   
+    }
+  
+}
+
+
+//Part 2 Thinking Methodically
+let 
