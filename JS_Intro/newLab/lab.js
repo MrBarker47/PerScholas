b5d6e3b1ec3f5b348ca4a264fe6463d8c0929c41
MrBarker47 <jebarker7@gmail.com>
2024-10-07 09:20:24 -0400

    Update files

--- JS_Intro/newLab/lab.js
+++ JS_Intro/newLab/lab.js
@@ -25,13 +25,25 @@ newArray();
 //Take an array of strings and return the longest string
 function str() {
     let newString = ['Money Talks', 'Friday', 'Rush Hour 2', 'Low Down Dirty Shame'];
-    let long = ' ';
+    let long = 0;
     for(let i = 0; i < newString.length; i++) {
-   
+            return long = newString[i];
     }
   
 }
 
+console.log(str());
 
 //Part 2 Thinking Methodically
-let 
+let container = [{id: "42", name: "Bruce", occupation: "Knight", age: "41"},
+                 {id: "48", name: "Barry", occupation: "Runner", age: "25"},
+                 {id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
+                 {id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
+                 {id: "7", name:   "Bilbo", occupation: "None", age: "111"}
+                ];
+
+                
+                container.sort((a, b) => a.age - b.age);
+                console.log(container);
+
+                //
