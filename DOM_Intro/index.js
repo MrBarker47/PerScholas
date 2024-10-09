e3807c1c9dd8a90e576799cc33f3be20919df7cd
MrBarker47 <jebarker7@gmail.com>
2024-10-09 13:30:33 -0400

    Update files

---
+++ DOM_Intro/index.js
@@ -0,0 +1,39 @@
+// let body = document.querySelector('body');
+// let div = document.createElement('div');
+// let div2 = document.createElement('div');
+// let ol = document.createElement('ol');
+// let ul = document.createElement('ul');
+
+
+// let li1 = document.createElement('li');
+// let li2 = document.createElement('li');
+// let li3 = document.createElement('li');
+// let li4 = document.createElement('li');
+// let li5 = document.createElement('li');
+// let li6 = document.createElement('li');
+
+
+
+
+// body.appendChild(div).appendChild(ol).appendChild(li1);
+// ol.appendChild(li2)
+// ol.appendChild(li3)
+
+
+// body.appendChild(div2).appendChild(ul).appendChild(li4)
+// ul.appendChild(li5);
+// ul.appendChild(li6);
+
+// ol.style.border = "4px solid blue";
+// ul.style.border = "4px solid red";
+
+let body = document.querySelector('body');
+let h1 = document.getElementById('html');
+let paragraph = document.createElement('p');
+
+
+
+h1.innerHTML = "Jamaal Barker";
+body.appendChild(paragraph).textContent = "Hello World";
+
+
