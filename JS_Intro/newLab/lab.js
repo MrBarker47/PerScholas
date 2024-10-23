2195566da1fde1811ec24018ff27d99b6927d123
MrBarker47 <jebarker7@gmail.com>
2024-10-23 09:56:54 -0400

    Update files

--- JS_Intro/newLab/lab.js
+++ JS_Intro/newLab/lab.js
@@ -15,61 +15,63 @@ function newArray() {
     let newList = [1,2,3,4];
     let avg = 0;
     for(let i = 0; i < newList.length; i++) {
-        newList.forEach((count) => avg - count);
+       avg += newList[i];
     }
-    console.log(avg);
+    console.log(avg / newList.length) ;
 }
 
 newArray();
 
 //Take an array of strings and return the longest string
-function str() {
-    let newString = ['Money Talks', 'Friday', 'Rush Hour 2', 'Low Down Dirty Shame'];
-    let long = 0;
-    for(let i = 0; i < newString.length; i++) {
-            return long = newString[i];
-    }
+// function str() {
+//     let newString = ['Money Talks', 'Friday', 'Rush Hour 2', 'Low Down Dirty Shame'];
+//     let long = 0;
+//     for(let i = 0; i < newString.length; i++) {
+//             return long = newString[i];
+//     }
   
-}
+// }
 
-console.log(str());
+// console.log(str());
 
 //Part 2 Thinking Methodically
-let container = [{id: "42", name: "Bruce", occupation: "Knight", age: "41"},
-                 {id: "48", name: "Barry", occupation: "Runner", age: "25"},
-                 {id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
-                 {id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
-                 {id: "7", name:   "Bilbo", occupation: "None", age: "111"}
-                ];
+// let container = [{id: "42", name: "Bruce", occupation: "Knight", age: "41"},
+//                  {id: "48", name: "Barry", occupation: "Runner", age: "25"},
+//                  {id: "57", name: "Bob", occupation: "Fry Cook", age: "19"},
+//                  {id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"},
+//                  {id: "7", name:   "Bilbo", occupation: "None", age: "111"}
+//                 ];
 
                 
-                container.sort((a, b) => a.age - b.age);
-                // console.log(container);
+//                 container.sort((a, b) => a.age - b.age);
+//                 // console.log(container);
 
         //filter container
 
-        let j = container.filter(remove => remove.age < 50);
+        // let j = container.filter(remove => remove.age < 50);
 
-        console.log(j);
+        // console.log(j);
     
 
         //Map Container
-        let e = container.map((change) => change.occupation = 'Job');
-        let t = container.map((a) => a.age - -1);
+        // let e = container.map((change) => change.occupation = 'Job');
+        // let t = container.map((a) => a.age - -1);
 
-        console.log(e);
-        console.log(t);
+        // console.log(e);
+        // console.log(t);
 
         //Reduce Container
-        let count = container.age;
-        let newCount = container.reduce((count, sum) => count += sum);
-        console.log(newCount);
+        // let count = container.age;
+        // let newCount = container.reduce((count, sum) => count += sum);
+        // console.log(newCount);
+        
+        
         //Thinking Criticaly 
-            function age() {
-                for(let i = 0; i < container.age; i++) {
-                    container.age += 1;
-                }
-                console.log(age);
-            }
+            // function age() {
+            //     for(let i = 0; i < container.age; i++) {
+            //         container.age += 1;
+            //     }
+            //     console.log(age);
+            // }
 
-            age();
+            // age();
