5d7335c44951e3da04e1595f212fe86a76d0ec53
MrBarker47 <jebarker7@gmail.com>
2024-10-27 12:10:03 -0400

    Update files

--- JS_Intro/newLab/lab.js
+++ JS_Intro/newLab/lab.js
@@ -3,24 +3,20 @@
 function array() {
     let list = [1,2,3,4,5,6,7];
     let sum = 0;
-    for(let i = 0; i < list.length; i++) {
         list.forEach((arr) => sum += arr);
-        }
-      console.log(sum);
+        console.log(sum);
     }
 array();
 
 //Take array of numbers and return the average.
 function newArray() {
-    let newList = [1,2,3,4];
-    let avg = 0;
-    for(let i = 0; i < newList.length; i++) {
-       avg += newList[i];
-    }
-    // console.log(avg / newList.length) ;
+    let newList = [1,2,3,4,5,6,7];
+   let average = newList.reduce((x, y) => x + y, 0);
+   let newAverage = average / newList.length;
+    console.log(newAverage);
 }
 
-// newArray();
+newArray();
 
 // Take an array of strings and return the longest string
 function str() {
@@ -33,9 +29,9 @@ function str() {
             long = newString[i];
         }
     }
-    // console.log(long);
+    console.log(long);
 }
-// console.log(str());
+console.log(str());
 
 
 
@@ -47,34 +43,36 @@ let container = [{id: "42", name: "Bruce", occupation: "Knight", age: "41"},
                  {id: "7", name:   "Bilbo", occupation: "None", age: "111"}
                 ];
 
+               
                 
-                // container.sort((a, b) => a.age - b.age);
-                // console.log(container);
+                container.sort((a, b) => a.age - b.age);
+        
 
         // filter container
-        // let j = container.filter(remove => remove.age < 50);
-        // console.log(j); 
+        let j = container.filter(remove => remove.age < 50);
+        console.log(j); 
     
 
         //Map Container
-        // let e = container.map((change) => change.occupation = 'Job');
-        // let t = container.map((a) => a.age - -1);
+        let upContainer = container.map(({occupation}) => ({Job:occupation}));
+        console.log(upContainer);
+        
+       let t = container.map((a) => a.age - -1);
+       console.log(t);
 
-        // console.log(e);
-        // console.log(t);
 
         //Reduce Container
-        // let count = container.age;
-        // let newCount = container.reduce((count, sum) => count += sum);
-        // console.log(newCount);
+        let count = container.age;
+        let newCount = container.reduce((count, sum) => count += sum);
+        console.log(newCount);
         
         
         //Thinking Criticaly 
-            // function age() {
-            //     for(let i = 0; i < container.age; i++) {
-            //         container.age += 1;
-            //     }
-            //     console.log(age);
-            // }
+            function age() {
+                for(let i = 0; i < container.age; i++) {
+                    container.age += 1;
+                }
+                console.log(age);
+            }
 
-            // age();
+            age();
